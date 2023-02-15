3b805b42786897797e043a4831942c4fa8bf0582
SShrimpman <camaraoacsn@gmail.com>
2023-02-15 12:50:27 +0000

    3D Simple Box

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import TitleExercise from './components/TitleExercise.vue'
+import ThreeJs from './components/ThreeJs.vue'
+</script>
+
+<template>
+  <TitleExercise/>
+  <ThreeJs/>
+</template>
---
+++ src/components/TitleExercise.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1 class="text-center m-5 text-2xl text-green-600">
+    ThreeJS + VueJS + ViteJS + TailwindCSS
+  </h1>
+</template>
